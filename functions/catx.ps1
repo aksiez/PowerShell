875f629ca13f1fd9cd480a5bab011091dcb4be54
axiez <siubohdan@gmail.com>
2026-03-12 14:05:38 +0200

    Added error handling

--- functions/catx.ps1
+++ functions/catx.ps1
@@ -1,5 +1,13 @@
 function catx {
     param($file)
-    if (-not (Test-Path $file)) { return }
-    [System.IO.File]::ReadAllText((Resolve-Path $file))
+    if (-not (Test-Path $file)) {
+        Write-Error "File '$file' does not exist."
+        return
+    }
+    try {
+        [System.IO.File]::ReadAllText((Resolve-Path $file))
+    }
+    catch {
+        Write-Error "Failed to read file '$file': $_"
+    }
 }
